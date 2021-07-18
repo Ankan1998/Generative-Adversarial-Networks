396a40e06d21fe8f67ebade52047f6a9bb90e091
Ankan Sharma <ankanroni3@gmail.com>
2021-07-18 20:47:41 +0530

    Structural change

--- GAN/discriminator.py
+++ GAN/discriminator.py
@@ -2,28 +2,28 @@ import torch
 import torch.nn as nn
 
 
-def disc_block(input_dim, output_dim):
-    return nn.Sequential(
-        nn.Linear(input_dim, output_dim),
-        nn.LeakyReLU(0.2)
-    )
-
-
 class Discriminator(nn.Module):
 
-    def __init__(self,in_dim=784, hid_dim=128):
+    def __init__(self, in_dim=784, hid_dim=128):
         super(Discriminator, self).__init__()
 
         self.disc = nn.Sequential(
-            disc_block(in_dim,hid_dim*4),
-            disc_block(hid_dim*4, hid_dim * 2),
-            disc_block(hid_dim * 2, hid_dim),
+            self.disc_block(in_dim, hid_dim * 4),
+            self.disc_block(hid_dim * 4, hid_dim * 2),
+            self.disc_block(hid_dim * 2, hid_dim),
             nn.Linear(hid_dim, 1),
 
         )
 
+    def disc_block(self, input_dim, output_dim):
+        return nn.Sequential(
+            nn.Linear(input_dim, output_dim),
+            nn.LeakyReLU(0.2)
+        )
+
     def forward(self, image):
         return self.disc(image)
+
 
 if __name__ == "__main__":
     pass
--- GAN/generator.py
+++ GAN/generator.py
@@ -1,28 +1,26 @@
 import torch
 import torch.nn as nn
 
-
-def gen_block(input_dim, output_dim):
-    return nn.Sequential(
-        nn.Linear(input_dim, output_dim),
-        nn.BatchNorm1d(output_dim),
-        nn.LeakyReLU(0.2)
-    )
-
-
 class Generator(nn.Module):
 
     def __init__(self, latent_noise_dim=10, in_dim=784, hid_dim=128):
         super(Generator, self).__init__()
 
         self.gen = nn.Sequential(
-            gen_block(latent_noise_dim,hid_dim),
-            gen_block(hid_dim, hid_dim * 2),
-            gen_block(hid_dim * 2, hid_dim * 4),
-            gen_block(hid_dim * 4, hid_dim * 8),
+            self.gen_block(latent_noise_dim,hid_dim),
+            self.gen_block(hid_dim, hid_dim * 2),
+            self.gen_block(hid_dim * 2, hid_dim * 4),
+            self.gen_block(hid_dim * 4, hid_dim * 8),
             nn.Linear(hid_dim * 8, in_dim),
             nn.Sigmoid()
 
+        )
+
+    def gen_block(self,input_dim, output_dim):
+        return nn.Sequential(
+            nn.Linear(input_dim, output_dim),
+            nn.BatchNorm1d(output_dim),
+            nn.LeakyReLU(0.2)
         )
 
     def forward(self, noise):
